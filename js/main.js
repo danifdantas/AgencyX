42cf2eb9325f07d068c43a0f666622d8c2abab08
Dani Dantas <dani@DaniVonRohr.local>
2018-11-14 20:41:15 -0500

    lightbox implemented with placeholders

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@
 
 let button = document.querySelectorAll(".button"),
   lightbox = document.querySelector(".lightbox"),
-  closeLB = document.querySelector(".close");
-
-var slides = document.querySelector(".mySlides"),
-  slide = document.querySelectorAll(".slides"),
-  demo = document.querySelectorAll(".demo"),
+  prev = document.querySelector("#prev"),
+  next = document.querySelector("#next"),
   caption = document.querySelector("#caption"),
-  prev = document.querySelector(".prev"),
-  next = document.querySelector(".next"),
-  pick = 1;
-showSlides(pick);
+  closeLB = document.querySelector(".close");
+const current = document.querySelector("#current");
+const imgs = document.querySelectorAll(".imgs img");
+const opacity = 0.6;
+var count = 0,
+  captions = ["Project 1", "Project 2", "Project 3", "Project 4"];
 
+imgs[0].style.opacity = opacity;
 // Functions
 function openLightBox() {
   lightbox.style.display = "block";
@@ -23,42 +23,45 @@ function closeLightBox() {
   //console.log('clicked');
   lightbox.style.display = "none";
 }
-function currentSlide(e) {
-  var pick = this.dataset.pick;
-  //console.log(pick);
-  showSlides(pick);
-}
+function imgClick(e) {
+  //reset the opacity
+  imgs.forEach(img => (img.style.opacity = 1));
+  //Change current image to src of clicked img
+  current.src = e.target.src;
+  // var position = e.target.index;
+  // console.log(position);
+  //caption.innerHTML = captions[pick];
+  //Add fade-in class
+  current.classList.add("fade-in");
 
-function prevSlides(e) {
-  var pick = this.dataset.pick;
-  showSlides(pick);
-}
-function showSlides(n) {
-  var i;
-  console.log(pick);
-  // if (n > slides.length) {
-  //   slideIndex = 1;
-  // }
-  // if (n < 1) {
-  //   slideIndex = slides.length;
-  // }
-  // for (i = 0; i < slides.length; i++) {
-  //   slides[i].style.display = "none";
-  // }
-  // for (i = 0; i < demo.length; i++) {
-  //   demo[i].className = demo[i].className.replace(" active", "");
-  // }
-  // slides[slideIndex - 1].style.display = "block";
-  // demo[slideIndex - 1].className += " active";
-  // caption.innerHTML = demo[slideIndex - 1].alt;
-}
+  //remove fade-in class
+  setTimeout(() => current.classList.remove("fade-in"), 500);
 
+  // Change the opacity
+  e.target.style.opacity = opacity;
+}
+function prevImg() {
+  count--;
+  if (count < 0) {
+    count = imgs.length - 1;
+  }
+  current.src = imgs[count].src;
+  caption.innerHTML = captions[count];
+}
+function nextImg() {
+  count++;
+  if (count == imgs.length) {
+    count = 0;
+  }
+  current.src = imgs[count].src;
+  caption.innerHTML = captions[count];
+}
 // Event Listeners
 
 closeLB.addEventListener("click", closeLightBox, false);
 for (var i = 0; i < button.length; i++) {
   button[i].addEventListener("click", openLightBox, false);
 }
-prev.addEventListener("click", prevSlides, false);
-next.addEventListener("click", prevSlides, false);
-demo.forEach(demo => demo.addEventListener("click", currentSlide));
+imgs.forEach(img => img.addEventListener("click", imgClick));
+prev.addEventListener("click", prevImg, false);
+next.addEventListener("click", nextImg, false);
